aa11aa6d73a6847d33116b26aece69d59162c660
chadGomez <chadegomez@yahoo.com>
2024-01-04 17:32:39 -0800

    substituting backend URL for Render.com deployment

--- src/App.js
+++ src/App.js
@@ -65,23 +65,37 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smartbrainws.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         input: this.state.input
       })
     })
+    // fetch('http://localhost:3000/imageurl', {
+    //   method: 'post',
+    //   headers: {'Content-Type': 'application/json'},
+    //   body: JSON.stringify({
+    //     input: this.state.input
+    //   })
+    // })
       .then(resp => resp.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrainws.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               id: this.state.user.id
             })
           })
+          // fetch('http://localhost:3000/image', {
+          //   method: 'put',
+          //   headers: {'Content-Type': 'application/json'},
+          //   body: JSON.stringify({
+          //     id: this.state.user.id
+          //   })
+          // })
             .then(response => response.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
