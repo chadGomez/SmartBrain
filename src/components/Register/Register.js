47a54290ef6193ac3c8b5c25b6065ab04171ade8
chadGomez <chadegomez@yahoo.com>
2024-01-10 23:14:06 -0800

    register error fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
   }
 
   onSubmitSignin = () => {
-    fetch('https://smartbrain-4mca.onrender.com/register', {
+    fetch('https://smartbrainws.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
